5dd5a109e950009eb2fa6c5c643c25bf155f110e
John Hobbs <john@velvetcache.org>
2014-11-19 17:14:08 -0600

    Initial Commit

---
+++ Junction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Junction
+//
+//  Created by John Hobbs on 11/19/14.
+//  Copyright (c) 2014 John Hobbs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Junction/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Junction
+//
+//  Created by John Hobbs on 11/19/14.
+//  Copyright (c) 2014 John Hobbs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
